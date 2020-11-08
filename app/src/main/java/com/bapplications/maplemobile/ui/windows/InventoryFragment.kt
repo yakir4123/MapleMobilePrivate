29a1f20c662b218495c9d28bf1c889a9f395fa7e
yakir4123 <38066978+yakir4123@users.noreply.github.com>
2020-11-08 17:54:10 +0200

    ViewPager2 for the inventory

--- app/src/main/java/com/bapplications/maplemobile/ui/windows/InventoryFragment.kt
+++ app/src/main/java/com/bapplications/maplemobile/ui/windows/InventoryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -11,11 +12,9 @@ import com.bapplications.maplemobile.R
 import com.bapplications.maplemobile.databinding.FragmentInventoryBinding
 import com.bapplications.maplemobile.gameplay.player.Player
 import com.bapplications.maplemobile.gameplay.player.inventory.InventoryType
-import com.bapplications.maplemobile.ui.adapters.InventoryAdapter
+import com.bapplications.maplemobile.ui.adapters.PageViewerInventoryAdapter
 import com.bapplications.maplemobile.ui.view_models.InventoryViewModel
-import com.bapplications.maplemobile.utils.BindingUtils
-import kotlinx.android.synthetic.main.fragment_inventory.*
-import onItemClick
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class InventoryFragment(private var player: Player) : Fragment() {
@@ -33,34 +32,57 @@ class InventoryFragment(private var player: Player) : Fragment() {
         viewModel.itemInventory
                 .postValue(player.inventory
                         .getInventory(viewModel.selectedInventoryType.value).items)
-        viewModel.itemInventory.addSource(viewModel.selectedInventoryType) {
-            value -> viewModel.itemInventory.postValue(player.inventory.getInventory(value).items)
+        viewModel.itemInventory.addSource(viewModel.selectedInventoryType) { value -> viewModel.itemInventory.postValue(player.inventory.getInventory(value).items)
         }
         viewModel.itemInventory
 
-        val adapter = InventoryAdapter()
-        binding.inventoryItemsRecycler.adapter = adapter
-
-        viewModel.itemInventory.observe(viewLifecycleOwner, {
-            loading_tv.visibility = View.GONE
-            adapter.data = it
-        })
-        binding.inventoryItemsRecycler.onItemClick { recview, position, v ->
-            val slot = (binding.inventoryItemsRecycler.adapter as InventoryAdapter).getSlot(position)
-            if(slot.item != null) {
-                val newFragment = ItemInfoFragment.newInstance(player, slot)
-                val transaction = parentFragmentManager.beginTransaction()
-                transaction.replace(R.id.tools_window, newFragment)
-                transaction.addToBackStack(null)
-                transaction.commit()
-            }
+        binding.inventoryPager.apply {
+            adapter = PageViewerInventoryAdapter(player.inventory)
         }
 
-        // swipes
-        binding.inventoryItemsRecycler.setOnHorizonSwipe { isLeft -> inventorySwipe(isLeft) }
-        viewModel.selectedInventoryType.observe(viewLifecycleOwner, {
-            binding.inventoryTypeButtonsGroup.check(BindingUtils.inventoryTypeToButtonId(it))
-        })
+        TabLayoutMediator(binding.selectedInventoryTab, binding.inventoryPager) { tab, position ->
+            tab.text = when(position) {
+                0 -> "Equip"
+                1 -> "Use"
+                2 -> "Set-Up"
+                3 -> "Etc"
+                4 -> "Cash"
+                else -> ""
+            }
+        }.attach()
+
+//        // Iterate over all tabs and set the custom view
+//        for (i in 0 until binding.selectedInventoryTab.tabCount) {
+//            val tab = binding.selectedInventoryTab.getTabAt(i)
+//            tab?.customView = LayoutInflater.from(context)
+//                    .inflate(R.layout.inventory_tab, null).apply {
+//                        this.findViewById<TextView>(R.id.tab_title).text = tab?.text
+//                    }
+//        }
+
+//        val adapter = RecyclerInventoryAdapter()
+//        binding.inventoryItemsRecycler.adapter = adapter
+//
+//        viewModel.itemInventory.observe(viewLifecycleOwner, {
+//            loading_tv.visibility = View.GONE
+//            adapter.data = it
+//        })
+//        binding.inventoryItemsRecycler.onItemClick { recview, position, v ->
+//            val slot = (binding.inventoryItemsRecycler.adapter as RecyclerInventoryAdapter).getSlot(position)
+//            if(slot.item != null) {
+//                val newFragment = ItemInfoFragment.newInstance(player, slot)
+//                val transaction = parentFragmentManager.beginTransaction()
+//                transaction.replace(R.id.tools_window, newFragment)
+//                transaction.addToBackStack(null)
+//                transaction.commit()
+//            }
+//        }
+//
+//        // swipes
+//        binding.inventoryItemsRecycler.setOnHorizonSwipe { isLeft -> inventorySwipe(isLeft) }
+//        viewModel.selectedInventoryType.observe(viewLifecycleOwner, {
+//            binding.inventoryTypeButtonsGroup.check(BindingUtils.inventoryTypeToButtonId(it))
+//        })
         return binding.root
     }
 
