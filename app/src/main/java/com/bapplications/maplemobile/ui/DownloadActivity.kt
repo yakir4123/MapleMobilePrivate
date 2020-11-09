155272c53eb5199cb37b7c71d6cb928b59beae42
neil <nilnil47@gmail.com>
2020-11-09 02:07:09 +0200

    download files from the app working

--- app/src/main/java/com/bapplications/maplemobile/ui/DownloadActivity.kt
+++ app/src/main/java/com/bapplications/maplemobile/ui/DownloadActivity.kt
@@ -9,17 +9,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.bapplications.maplemobile.R
-import okhttp3.Call
-import okhttp3.Callback
 import okhttp3.Response
 import okio.Buffer
 import okio.BufferedSink
 import okio.buffer
 import okio.sink
 import java.io.File
-import java.io.IOException
 import kotlin.concurrent.thread
-
 
 val files = arrayOf(
         "String.nx",
@@ -51,6 +47,22 @@ class DownloadActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        files.forEach {
+            if (!File("${this.getExternalFilesDir(null)}/$it").exists()) {
+                keepToNextActivity = false
+            }
+        }
+
+        if (keepToNextActivity) {
+            startActivity(Intent(this, GameActivity::class.java))
+        } else {
+            setUpView()
+            downloadFiles()
+        }
+    }
+
+    fun setUpView() {
         setContentView(R.layout.activity_download)
 
         downloadingTv = findViewById(R.id.downloadingTv)
@@ -68,66 +80,29 @@ class DownloadActivity : AppCompatActivity() {
 
         })
 
-        currentFile.observe(this, Observer { file -> currentFileTv.setText(file)})
-
-        downloadFiles()
-
-//        files.forEach {
-//            if (!File("${this.getExternalFilesDir(null)}/$it").exists()) {
-//                keepToNextActivity = false
-//            }
-//        }
-//
-//        if (keepToNextActivity) {
-//            startActivity(Intent(this, GameActivity::class.java))
-//        } else {
-//            downloadFiles()
-//        }
+        currentFile.observe(this, Observer { file -> currentFileTv.setText(file) })
     }
 
     fun downloadFiles() {
+
         thread(start = true) {
-//            downloadFile("http://137.135.90.47/Charecter.nx", File(getExternalFilesDir(null)!!, "Charecter.nx"))
             files.forEach {
+//                downloadFile("http://192.168.1.18/$it", File(getExternalFilesDir(null)!!, it))
                 downloadFile("http://137.135.90.47/$it", File(getExternalFilesDir(null)!!, it))
             }
+
         }
-//        downloadManager = this.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-//        com.bapplications.maplemobile.utils.files.forEach {
-//            if (!File("${this.getExternalFilesDir(null)}$it").exists()) {
-//                Log.d("DownloadingManager", "downloadAssets: $it: http://${Configuration.FILES_HOST}$it")
-//                downloadFile("https://${Configuration.FILES_HOST}/$it", getExternalFilesDir(null)!!)
-//                val request = DownloadManager.Request(
-//                        Uri.parse("http://${Configuration.FILES_HOST}/$it"))
-//                        .setTitle("MapleMobile")
-//                        .setDescription("downloading $it")
-//                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-//                        .setDestinationUri(Uri.parse("file://${this.getExternalFilesDir(null)}/$it"))
-//                downloadManager.enqueue(request)
-//            }
-//        }
     }
 
     fun downloadFile(url: String, path: File) {
+        Log.d(TAG, "downloadFile: downloading: $url, size $fileSize")
         currentFile.postValue(url)
         val request: okhttp3.Request = okhttp3.Request.Builder()
                 .url(url)
                 .build()
 
-        client.newCall(request).enqueue(
-                object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        Log.d(TAG, "onFail: " + e)
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        Log.d(TAG, "onResponse: ")
-                        saveFile(response, path)
-                    }
-                }
-        )
-//        val response: okhttp3.Response = client.newCall(request).execute()
-
+        // we are using the synchronous
+        saveFile(client.newCall(request).execute(), path)
     }
 
     private fun saveFile(response: Response, path: File) {
@@ -136,14 +111,13 @@ class DownloadActivity : AppCompatActivity() {
 
         val source = response.body!!.source()
         fileSize = response.body!!.contentLength()
-        Log.d(TAG, "file size " + fileSize)
         val buffer = Buffer()
 
         while (source.read
                 (buffer, CHUNK_SIZE).also {
                     byteCount = it
-                    progress.value!!.plus(byteCount)
                     textProgress.postValue(textProgress.value!! + byteCount)
+                    Log.d(TAG, "saveFile: $byteCount")
                 } != -1L) {
             sink.write(buffer, byteCount)
             sink.flush()
